dd400cd82699176d1fe42e32d9ab2454b158d9ab
Urs Voegele <unext@bluewin.ch>
2020-12-31 16:42:55 +0100

    new basebox version 3.0.1609428787

--- inspec/baseline-ubuntu-18.04-docker/controls/check_docker.rb
+++ inspec/baseline-ubuntu-18.04-docker/controls/check_docker.rb
@@ -2,15 +2,12 @@
 
 title "check docker"
 
-# check docker version
-control "tmp-1.0" do                        # A unique ID for this control
+# check docker service
+control "docker-1.0" do                     # A unique ID for this control
   impact 1.0                                # The criticality, if this control fails.
-  title "Check docker version"              # A human-readable title
-  desc "Check docker version"
-  describe docker.version do
-    its('Server.Version') { should cmp >= '20.10'}
-    its('Client.Version') { should cmp >= '20.10'}
-    its('Server.Version') { should cmp <  '21.00'}
-    its('Client.Version') { should cmp <  '21.00'}
+  title "check if docker is installed"      # A human-readable title
+  desc "check docker service"
+  describe packages(/docker/) do           # The actual test
+    its('statuses') { should cmp 'installed' } 
   end
 end
